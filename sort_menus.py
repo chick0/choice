f4de40a260621504a25f04a0474fab13b964d31b
chick_0 <chick_0@ch1ck.xyz>
2022-03-07 02:18:28 +0900

    print length

--- sort_menus.py
+++ sort_menus.py
@@ -7,5 +7,6 @@ path = join("app", "menus.json")
 json = load(open(path, mode="r", encoding="utf-8"))
 json = list(set(json))
 json = sorted(json)
+print(len(json))
 
 dump(json, open(path, mode="w", encoding="utf-8"), ensure_ascii=False, indent=4)
