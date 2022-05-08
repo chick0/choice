8d4df174ec9377a2305786e7ea0dcf9c22d79be2
chick_0 <chick_0@ch1ck.xyz>
2022-05-08 23:39:15 +0900

    tool fixed

--- menu.py
+++ menu.py
@@ -1,19 +1,19 @@
-from json import load
+from json import loads
 from json import dump
 from version import PATH_JSON
 from version import update_version
 
 
 def main():
-    menu = input("새로운 메뉴를 입력해주세요 : ")
-    json: list = load(open(PATH_JSON, mode="r", encoding="utf-8"))
+    menu = input("새로운 메뉴를 입력해주세요 : ").strip()
+    json = loads(open(PATH_JSON, mode="r", encoding="utf-8").read())
 
     if menu in json:
         print("해당 메뉴는 이미 등록된 메뉴 입니다.")
     else:
-        json.append(sorted(menu))
+        json.append(menu)
         dump(
-            menu,
+            sorted(json),
             open(PATH_JSON, mode="w", encoding="utf-8"),
             ensure_ascii=False,
         )
